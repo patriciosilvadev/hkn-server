e55616ae8feb900744577cd5954dfe02abef3759
Frank Ye <frankyyzy@gmail.com>
2021-02-10 18:35:39 -0800

    Update TeapotController.ts

--- src/controllers/TeapotController.ts
+++ src/controllers/TeapotController.ts
@@ -1,10 +1,21 @@
 import { JsonController, Get, HttpCode } from 'routing-controllers';
+import logger from '../logging';
 
 @JsonController('/api/teapot')
 export class TeapotController {
   @Get('/')
   @HttpCode(418)
   async getEvent(): Promise<string> {
+    logger.log({
+      level: 'info',
+      message: 'teapot info',
+      tags: 'testing',
+    });
+    logger.log({
+      level: 'debug',
+      message: 'teapot info',
+      tags: 'testing',
+    });
     return 'I am a teapot!';
   }
 }
